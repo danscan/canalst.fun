e1f063a34339d5f5c2da36ab28f460e4619e6bee
Dan Scanlon <dscanlonpa@gmail.com>
2021-07-23 09:39:07 -0400

    Add useNft

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import { getDefaultProvider, Contract } from 'ethers';
+import { NftProvider } from 'use-nft';
 import 'tailwindcss/tailwind.css'
 import '../styles/app.css';
 
+const nftFetcher = ['ethers', { ethers: { Contract }, provider: getDefaultProvider() }];
+
 export default function CustomApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (!process.browser) {
+    return <Component {...pageProps} />;
+  }
+
+  return (
+    <NftProvider fetcher={nftFetcher}>
+      <Component {...pageProps} />
+    </NftProvider>
+  );
 }
