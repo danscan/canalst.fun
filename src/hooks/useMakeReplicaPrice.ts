ed15eedafa4fe4836c8208e5504475c96a4e7815
Dan Scanlon <dscanlonpa@gmail.com>
2021-08-07 03:03:01 -0400

    Add useMakeReplicaPrice

---
+++ src/hooks/useMakeReplicaPrice.ts
@@ -0,0 +1,57 @@
+import { BigNumber, Contract, ContractFunction, ethers } from 'ethers';
+import { useAsync } from 'react-use';
+import CanalStFun from '../../contracts/artifacts/CanalStFun.json';
+import ChainlinkPriceFeedABI from '../abis/ChainlinkPriceFeedABI.json';
+import { providerEthereum, providerPolygon } from '../constants/provider';
+
+export default function useMakeReplicaPrice(resolvedProvider: ethers.providers.BaseProvider) {
+  return useAsync(async () => {
+    if (!resolvedProvider) {
+      return null;
+    }
+
+    const network = await resolvedProvider.getNetwork();
+    const { chainId } = network;
+    console.log('chainId', chainId);
+
+    let canalStFunContract: CanalStFunContract;
+    let chainNativeTokenPriceContract: ChainlinkPriceFeedContract;
+    switch (chainId) {
+      case 1:
+        canalStFunContract = new ethers.Contract(process.env.NEXT_PUBLIC_CANAL_ST_FUN_CONTRACT_ADDRESS_ETHEREUM, CanalStFun.abi, providerEthereum) as CanalStFunContract;
+        chainNativeTokenPriceContract = new ethers.Contract('0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419', ChainlinkPriceFeedABI, providerEthereum) as ChainlinkPriceFeedContract;
+        break;
+      case 137:
+        canalStFunContract = new ethers.Contract(process.env.NEXT_PUBLIC_CANAL_ST_FUN_CONTRACT_ADDRESS_POLYGON, CanalStFun.abi, providerPolygon) as CanalStFunContract;
+        chainNativeTokenPriceContract = new ethers.Contract('0x7bAC85A8a13A4BcD8abb3eB7d6b4d632c5a57676', ChainlinkPriceFeedABI, providerEthereum) as ChainlinkPriceFeedContract;
+        break;
+      default:
+        throw new Error('Wrong network. Please use the Ethereum (1) or Polygon (137) chain.');
+    }
+    
+    const [
+      canalStFunMakeReplicaPrice, // native token
+      chainNativeTokenPrice, // usd+e8
+      chainNativeTokenPriceDecimals // e8,
+    ] = await Promise.all([
+      canalStFunContract.makeReplicaPrice(),
+      chainNativeTokenPriceContract.latestAnswer(),
+      chainNativeTokenPriceContract.decimals(),
+    ]);
+
+    const chainNativeTokenPriceUSD = Number(ethers.utils.formatUnits(chainNativeTokenPrice, chainNativeTokenPriceDecimals));
+    const canalStPriceNative = Number(ethers.utils.formatEther(canalStFunMakeReplicaPrice));
+    const canalStPriceUSD = chainNativeTokenPriceUSD / canalStPriceNative;
+
+    return canalStPriceUSD;
+  }, [resolvedProvider]);
+}
+
+type CanalStFunContract = InstanceType<typeof Contract> & {
+  makeReplicaPrice: ContractFunction<BigNumber>;
+}
+
+type ChainlinkPriceFeedContract = InstanceType<typeof Contract> & {
+  decimals: ContractFunction<number>;
+  latestAnswer: ContractFunction<BigNumber>;
+}
